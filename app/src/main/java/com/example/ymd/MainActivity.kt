51142b89fe8d8318ae6b5c855ddaa3b078ac0251
kku <ruddns5315@gmail.com>
2023-09-26 10:17:46 +0900

    [FEAT] : ⚙️: 탭 레이아웃 검색항목 추가

--- app/src/main/java/com/example/ymd/MainActivity.kt
+++ app/src/main/java/com/example/ymd/MainActivity.kt
@@ -7,14 +7,12 @@ import com.example.ymd.databinding.ActivityMainBinding
 import com.example.ymd.databinding.FragmentHomeBinding
 import com.example.ymd.databinding.FragmentHotBinding
 import com.example.ymd.databinding.FragmentMypageBinding
+import com.example.ymd.videoList.SearchFragment
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    lateinit var tab1: FragmentHomeBinding
-    lateinit var tab2: FragmentHotBinding
-    lateinit var tab3: FragmentMypageBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +21,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initViewPager()
+
+        binding.mainNotification.setOnClickListener {
+
+        }
     }
 
     private fun initViewPager() {
@@ -30,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         viewPager2Adatper.addFragment(HomeFragment())
         viewPager2Adatper.addFragment(HotFragment())
         viewPager2Adatper.addFragment(MypageFragment())
+        viewPager2Adatper.addFragment(SearchFragment())
 
         binding.mainViewpager.apply {
             adapter = viewPager2Adatper
@@ -43,9 +46,10 @@ class MainActivity : AppCompatActivity() {
 
         TabLayoutMediator(binding.tableLayout, binding.mainViewpager) { tab, position ->
             when (position) {
-                0 -> tab.text = "Home"
-                1 -> tab.text = "Hot"
-                2 -> tab.text = "Mypage"
+                0 -> tab.setIcon(R.drawable.home).text = "Home"
+                1 -> tab.setIcon(R.drawable.hot).text = "Hot"
+                2 -> tab.setIcon(R.drawable.mymedia).text = "Mypage"
+                3 -> tab.setIcon(R.drawable.search).text = "Search"
             }
         }.attach()
     }
