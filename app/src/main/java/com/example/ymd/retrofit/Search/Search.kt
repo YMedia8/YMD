36b7ab2c19a6ab4a1428527ecd5b8bb987615101
kku <ruddns5315@gmail.com>
2023-09-26 21:52:57 +0900

    [FEAT] : ⚙️: 데이터 클래스 추가

---
+++ app/src/main/java/com/example/ymd/retrofit/Search/Search.kt
@@ -0,0 +1,92 @@
+package com.example.ymd.retrofit.Search
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Search(
+    @SerializedName("etag")
+    val etag: String,
+    @SerializedName("items")
+    val items: List<Item>,
+    @SerializedName("kind")
+    val kind: String,
+    @SerializedName("nextPageToken")
+    val nextPageToken: String,
+    @SerializedName("pageInfo")
+    val pageInfo: PageInfo,
+    @SerializedName("regionCode")
+    val regionCode: String
+)
+
+data class Snippet(
+    @SerializedName("channelId")
+    val channelId: String,
+    @SerializedName("channelTitle")
+    val channelTitle: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("liveBroadcastContent")
+    val liveBroadcastContent: String,
+    @SerializedName("publishTime")
+    val publishTime: String,
+    @SerializedName("publishedAt")
+    val publishedAt: String,
+    @SerializedName("thumbnails")
+    val thumbnails: Thumbnails,
+    @SerializedName("title")
+    val title: String
+)
+data class Thumbnails(
+    @SerializedName("default")
+    val default: Default,
+    @SerializedName("high")
+    val high: High,
+    @SerializedName("medium")
+    val medium: Medium
+)
+data class PageInfo(
+    @SerializedName("resultsPerPage")
+    val resultsPerPage: Int,
+    @SerializedName("totalResults")
+    val totalResults: Int
+)
+data class Medium(
+    @SerializedName("height")
+    val height: Int,
+    @SerializedName("url")
+    val url: String,
+    @SerializedName("width")
+    val width: Int
+)
+data class Item(
+    @SerializedName("etag")
+    val etag: String,
+    @SerializedName("id")
+    val id: Id,
+    @SerializedName("kind")
+    val kind: String,
+    @SerializedName("snippet")
+    val snippet: Snippet
+)
+data class Id(
+    @SerializedName("kind")
+    val kind: String,
+    @SerializedName("videoId")
+    val videoId: String
+)
+data class High(
+    @SerializedName("height")
+    val height: Int,
+    @SerializedName("url")
+    val url: String,
+    @SerializedName("width")
+    val width: Int
+)
+data class Default(
+    @SerializedName("height")
+    val height: Int,
+    @SerializedName("url")
+    val url: String,
+    @SerializedName("width")
+    val width: Int
+)
